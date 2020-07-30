17f1147cbbb2f7e7fea3f6d3826ce1ffc98feb0f
VinaAlv <68529774+VinaAlv@users.noreply.github.com>
2020-07-31 01:38:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $sheep = new Animal("shaun");
 
 echo $sheep->get_name(); echo '<br>'; // "shaun"
 echo $sheep->get_legs(); echo '<br>';// 2
-echo $sheep->get_cold_blooded() ;echo '<br>'; // false
+echo $sheep->get_cold_blooded() ; // false
 
 $sungokong = new Ape("kera sakti");
 $sungokong->yell() ;echo '<br>'; // "Auooo"
